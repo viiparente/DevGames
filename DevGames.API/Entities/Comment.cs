82ca6b5b921c3c148cd55a629e694b348b86e663
Vinicius Parente <vd.parente@hotmail.com>
2022-02-06 21:58:08 -0300

    Created Entities

---
+++ DevGames.API/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿namespace DevGames.API.Entities
+{
+    public class Comment
+    {
+        public Comment(string title, string description, string user, int postId)
+        {
+            Title = title;
+            Description = description;
+            User = user;
+            PostId = postId;
+
+            CreatedAt = DateTime.Now;
+        }
+
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string User { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public int PostId { get; private set; }
+    }
+}
---
+++ DevGames.API/Entities/Post.cs
@@ -0,0 +1,29 @@
+﻿namespace DevGames.API.Entities
+{
+    public class Post
+    {
+        public Post(string title, string description, int boardId)
+        {
+            Title = title;
+            Description = description;
+            BoardId = boardId;
+
+            CreatedAt = DateTime.Now;
+            Comments = new List<Comment>();
+        }
+
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public int BoardId { get; private set; }
+
+        public List<Comment> Comments { get; private set; }
+
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+    }
+}
