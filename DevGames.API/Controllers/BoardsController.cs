795597e7c49c7f34ef883ef5d11bf8770fd7dedc
Vinicius Parente <vd.parente@hotmail.com>
2022-02-06 21:18:52 -0300

    Created Controllers Boards and Posts

---
+++ DevGames.API/Controllers/BoardsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevGames.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        // PUT api/boards/1
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return NoContent();
+        }
+
+        // Delete api/boards/1
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevGames.API/Controllers/PostsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevGames.API.Controllers
+{
+
+    [Route("api/boards/{id}/posts")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        // api/boards/1/posts
+        [HttpGet]
+        public IActionResult GetAll(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{postId}")]
+        public IActionResult GetById(int id, int postId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(int id)
+        {
+            return Ok();
+        }
+
+        // POST - api/boards/1/posts/1/comments 
+        [HttpPost("{postId}/comments")]
+        public IActionResult PostComment(int id, int postId)
+        {
+            return NoContent();
+        }
+    }
+}
