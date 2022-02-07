0f0feb526c945524a6be2b2e199e62f1b84b50bf
Vinicius Parente <vd.parente@hotmail.com>
2022-02-06 22:21:59 -0300

    DbContext configuration and initialization

--- DevGames.API/Program.cs
+++ DevGames.API/Program.cs
@@ -1,6 +1,24 @@
+using DevGames.API.Persistence.Repositories;
+using DevGames.API.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DevGamesCs");
+
+//builder.Services
+//    .AddDbContext<DevGamesContext>(o => 
+//        o.UseSqlServer(connectionString));
+
+builder.Services
+    .AddDbContext<DevGamesContext>(o =>
+        o.UseInMemoryDatabase("DevGamesDb"));
+
+builder.Services.AddScoped<IBoardRepository, BoardRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
